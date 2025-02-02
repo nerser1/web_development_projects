8e8da0a624ae26958c847a08bd2289ece577042a
nerser1 <serinerya@gmail.com>
2025-02-02 10:48:50 +0200

    Add files via upload

---
+++ DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Nerya";
+document.querySelector("li a").innerHTML = "nerser";
+document.querySelector("button").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge");
